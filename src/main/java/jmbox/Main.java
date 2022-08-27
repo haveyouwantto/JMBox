636ec75645a7ae8a60da15c264f21470cbbc6f96
haveyouwantto <haveyouwantto@gmail.com>
2022-08-28 01:22:00 +0800

    add custom port

--- src/main/java/jmbox/Main.java
+++ src/main/java/jmbox/Main.java
@@ -3,17 +3,18 @@ package jmbox;
 import jmbox.web.Config;
 import jmbox.web.WebServer;
 
-import javax.sound.midi.MidiUnavailableException;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Config.load();
         int port = 64000;
-        if (args.length>=2){
-            port = Integer.parseInt(args[1]);
+        System.out.println(Arrays.toString(args));
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
         }
         WebServer server = new WebServer(new File("."), new InetSocketAddress(port));
         server.start();
