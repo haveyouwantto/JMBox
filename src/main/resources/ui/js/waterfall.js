0a359ac7d899b1700a11e25b5f9374252767b0de
haveyouwantto <haveyouwantto@gmail.com>
2022-12-26 09:00:28 +0800

    add keyboard

--- src/main/resources/ui/js/waterfall.js
+++ src/main/resources/ui/js/waterfall.js
@@ -14,6 +14,12 @@ let palette = [
 ]
 
 let spanDuration = 4;
+let noteWidth = canvas.width / 128;
+let keyboardHeight = noteWidth * 10;
+let blackKeyHeight = noteWidth * 6;
+const shiftval = 2 - 12 / 7;
+
+let notes = Array(128);
 
 // Entrance to waterfall
 controlsLeft.addEventListener('click', e => {
@@ -25,6 +31,25 @@ controlsLeft.addEventListener('click', e => {
     }
     resizeCanvas();
 });
+
+function isBlackKey(midiNoteNumber) {
+    // 定义一个包含所有黑键的数组
+    const blackKeys = [1, 3, 6, 8, 10];
+    // 计算 MIDI 音高编号对应的音符名称的编号（0-11）
+    const noteNameNumber = midiNoteNumber % 12;
+    // 如果音符名称的编号在 blackKeys 数组中，则该音符为黑键
+    return blackKeys.includes(noteNameNumber);
+}
+
+function getWhiteKeyNumber(midiNoteNumber) {
+    // 计算 MIDI 音高编号对应的音符名称的编号（0-11）
+    const noteNameNumber = midiNoteNumber % 12;
+    const mul = parseInt(midiNoteNumber / 12);
+    // 定义一个数组，包含所有白键的音符名称的编号
+    const whiteKeyNumbers = [0, 2, 4, 5, 7, 9, 11];
+    // 返回白键的编号（从 1 开始）
+    return whiteKeyNumbers.indexOf(noteNameNumber) + mul * 7;
+  }
 
 
 function draw() {
@@ -40,12 +65,44 @@ function draw() {
             )) {
                 let startY = (note.startTime - playTime) / spanDuration * canvas.height;
                 let endY = (note.stopTime - playTime) / spanDuration * canvas.height;
-                let width = canvas.width / 128;
-                let x = note.pitch * width;
+                let x = note.pitch * noteWidth;
 
-                canvasCtx.fillRect(x, canvas.height - endY, width, endY - startY)
+                canvasCtx.fillRect(x, canvas.height - endY - keyboardHeight, noteWidth, endY - startY);
+
+                if (note.startTime < playTime && note.stopTime > playTime) {
+                    notes[note.pitch] = i;
+                }
             }
         }
+
+        canvasCtx.fillStyle = 'white';
+        for (let i = 0; i < 128; i++) {
+            if (!isBlackKey(i)) {
+                if (notes[i] != null) {
+                    canvasCtx.fillStyle = palette[notes[i]];
+                    notes[i] = null;
+                }
+                let x = getWhiteKeyNumber(i) * (12 / 7);
+                canvasCtx.fillRect(noteWidth * x, canvas.height - keyboardHeight, noteWidth * (12 / 7), keyboardHeight);
+                canvasCtx.fillStyle = 'white';
+            }
+        }
+
+        canvasCtx.fillStyle = 'black';
+        for (let i = 0; i < 128; i++) {
+            if (isBlackKey(i)) {
+                if (notes[i] != null) {
+                    canvasCtx.fillStyle = palette[notes[i]];
+                    notes[i] = null;
+                }
+                canvasCtx.fillRect(i * noteWidth, canvas.height - keyboardHeight, noteWidth, blackKeyHeight);
+                canvasCtx.fillStyle = 'black';
+            }
+        }
+
+        canvasCtx.fillStyle = '#b71c1c';
+        canvasCtx.fillRect(0, canvas.height - keyboardHeight - noteWidth * 0.5, canvas.width, noteWidth * 0.5);
+
         requestAnimationFrame(draw);
     }
 }
@@ -53,6 +110,10 @@ function draw() {
 function resizeCanvas() {
     let { width: cssWidth, height: cssHeight } = canvas.getBoundingClientRect();
     canvas.width = dpr * cssWidth;
-    canvas.height =  dpr * cssHeight;
+    canvas.height = dpr * cssHeight;
+
+    noteWidth = canvas.width / 128;
+    keyboardHeight = noteWidth * 10;
+    blackKeyHeight = noteWidth * 6;
 }
 onresize = resizeCanvas;
